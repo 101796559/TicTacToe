d9620356d241fd7c2022c979e3512b5a1ec53f8e
101796559 <101796559@student.swin.edu.au>
2020-03-04 10:02:59 +1100

    'ai' smarter - still no draw condition

--- Program.cs
+++ Program.cs
@@ -33,10 +33,10 @@ namespace TicTacToe
                 System.Console.WriteLine("Player {0}\n", mark);
 
                 new Program().ShowBoard();
-                if (player == 1) {   
+                if (player == 1 && !over) {   
                     new Program().PlayerSelection();
                 }
-                else {
+                else if (!over) {
                     new Program().AiSelection();
                 }
             }
@@ -44,7 +44,8 @@ namespace TicTacToe
             Console.Clear();
             System.Console.WriteLine("Game over!\n");
             new Program().ShowBoard();
-            if (player == 3) player = 1;
+            if (player == 3 || player == 2) player -= 1;
+            else player += 1;
             System.Console.WriteLine("\nPlayer {0} wins!", player);
         }
 
@@ -84,8 +85,93 @@ namespace TicTacToe
         // }
         void AiSelection() {
             // Horizontal
+            // Row 1 AI
+            if (choices[1] == mark && choices[1] == choices[2] && choices[3] == "3") {
+                choices[3] = mark;
+            }
+            else if (choices[1] == mark && choices[1] == choices[3] && choices[2] == "2") {
+                choices[2] = mark;
+            }
+            else if (choices[2] == mark && choices[2] == choices[3] && choices[1] == "1") {
+                choices[1] = mark;
+            }
+
+            // Row 2 AI
+            else if (choices[4] == mark && choices[4] == choices[5] && choices[6] == "6") {
+                choices[6] = mark;
+            }
+            else if (choices[4] == mark && choices[4] == choices[6] && choices[5] == "5") {
+                choices[5] = mark;
+            }
+            else if (choices[5] == mark && choices[5] == choices[6] && choices[4] == "4") {
+                choices[4] = mark;
+            }
+            
+            // Row 3 AI
+            else if (choices[7] == mark && choices[7] == choices[8] && choices[9] == "9") {
+                choices[9] = mark;
+            }
+            else if (choices[7] == mark && choices[7] == choices[9] && choices[8] == "8") {
+                choices[8] = mark;
+            }
+            else if (choices[8] == mark && choices[8] == choices[9] && choices[7] == "7") {
+                choices[7] = mark;
+            }
+
+            // Diagonal AI
+            else if (choices[1] == mark && choices[1] == choices[5] && choices[9] == "9") {
+                choices[9] = mark;
+            }
+            else if (choices[1] == mark && choices[1] == choices[9] && choices[5] == "5") {
+                choices[5] = mark;
+            }
+            else if (choices[5] == mark && choices[5] == choices[9] && choices[1] == "1") {
+                choices[1] = mark;
+            }
+            else if (choices[3] == mark && choices[3] == choices[5] && choices[7] == "7") {
+                choices[7] = mark;
+            }
+            else if (choices[3] == mark && choices[3] == choices[7] && choices[5] == "5") {
+                choices[5] = mark;
+            }
+            else if (choices[5] == mark && choices[5] == choices[7] && choices[3] == "3") {
+                choices[3] = mark;
+            }
+
+            // Vertical AI
+            else if (choices[1] == mark && choices[1] == choices[4] && choices[7] == "7") {
+                choices[7] = mark;
+            }
+            else if (choices[1] == mark && choices[1] == choices[7] && choices[4] == "4") {
+                choices[4] = mark;
+            }
+            else if (choices[4] == mark && choices[4] == choices[7] && choices[1] == "1") {
+                choices[1] = mark;
+            }
+
+            else if (choices[2] == mark && choices[2] == choices[5] && choices[8] == "8") {
+                choices[8] = mark;
+            }
+            else if (choices[2] == mark && choices[2] == choices[8] && choices[5] == "5") {
+                choices[5] = mark;
+            }
+            else if (choices[5] == mark && choices[5] == choices[8] && choices[2] == "2") {
+                choices[2] = mark;
+            }
+
+            else if (choices[3] == mark && choices[3] == choices[6] && choices[9] == "9") {
+                choices[9] = mark;
+            }
+            else if (choices[3] == mark && choices[3] == choices[9] && choices[6] == "6") {
+                choices[6] = mark;
+            }
+            else if (choices[6] == mark && choices[6] == choices[9] && choices[3] == "3") {
+                choices[3] = mark;
+            }
+
+            // Horizontal
             // Row 1
-            if (choices[1] == choices[2] && choices[3] == "3") {
+            else if (choices[1] == choices[2] && choices[3] == "3") {
                 choices[3] = mark;
             }
             else if (choices[1] == choices[3] && choices[2] == "2") {
@@ -127,7 +213,6 @@ namespace TicTacToe
             else if (choices[5] == choices[9] && choices[1] == "1") {
                 choices[1] = mark;
             }
-
             else if (choices[3] == choices[5] && choices[7] == "7") {
                 choices[7] = mark;
             }
@@ -171,15 +256,48 @@ namespace TicTacToe
 
             // Until win condition almost met
             else {
-                while (!validSelection && !over) {
+                if (choices[5] == "5"){
+                    choices[5] = mark;
+                }
+                else if (choices[1] != "1" && choices[2] == "2" && choices[3] == "3" && choices[4] == "4" && choices[5] == "5" && choices[6] == "6" && choices[7] == "7" && choices[8] == "8" && choices[9] == "9") {
+                    choices[5] = mark;
+                }
+                else if (choices[3] != "3" && choices[2] == "2" && choices[1] == "1" && choices[4] == "4" && choices[5] == "5" && choices[6] == "6" && choices[7] == "7" && choices[8] == "8" && choices[9] == "9") {
+                    choices[5] = mark;
+                }
+                else if (choices[7] != "7" && choices[2] == "2" && choices[1] == "1" && choices[4] == "4" && choices[5] == "5" && choices[6] == "6" && choices[3] == "3" && choices[8] == "8" && choices[9] == "9") {
+                    choices[5] = mark;
+                }
+                else if (choices[9] != "9" && choices[2] == "2" && choices[1] == "1" && choices[4] == "4" && choices[5] == "5" && choices[6] == "6" && choices[3] == "3" && choices[8] == "8" && choices[7] == "7") {
+                    choices[5] = mark;
+                }
+                else if (choices[5] != "5" && choices[2] == "2" && choices[1] == "1" && choices[4] == "4" && choices[3] == "3" && choices[6] == "6" && choices[7] == "7" && choices[8] == "8" && choices[9] == "9") {
                     choice = random.Next(1,9);
-                    if (choices[choice] != "X" && choices[choice] != "O") {
-                        validSelection = true;
-                    } else {
-                        System.Console.WriteLine("{0} already has that spot!", choices[choice]);
+                    if (choice % 2 == 1 && choice != 5) {
+                        choices[choice] = mark;
+                    }
+                    else {
+                        choices[choice + 1] = mark;
                     }
                 }
-                choices[choice] = mark;
+                else if ((choices[1] != mark || choices[1] != "1") && (choices[9] != mark || choices[9] != "9")) {
+                    choice = random.Next(1,9);
+                    if ((choice % 2 != 1 && choices[choice] != "X" && choices[choice] != "O") || choices[choice] == "9") {
+                        choices[choice] = mark;
+                    }
+                    else if (choices[choice + 1] != "X" && choices[choice + 1] != "O") {
+                        choices[choice + 1] = mark;
+                    }
+                }
+                else {
+                    while (!validSelection && !over) {
+                        choice = random.Next(1,9);
+                        if (choices[choice] != "X" && choices[choice] != "O") {
+                            validSelection = true;
+                        } 
+                    }
+                    choices[choice] = mark;
+                }
             }
             player++;
         }
